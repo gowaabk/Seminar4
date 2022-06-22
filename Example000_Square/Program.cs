e04eb62229f15e02384f9fe5e0d936eb262716ab
Gowaa <gowaa@bk.ru>
2022-06-22 15:02:16 +0300

    Add Square

---
+++ Example000_Square/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат
+// (число умноженное на само себя).
+
+Console.Write("VVedite chislo "); //Вывод в консоль приглашения
+string chislo = Console.ReadLine(); // Ввод строки с консоли
+int number = int.Parse(chislo); // Преобразование строки в целое число
+int result = number * number; // Вычисление квадрата
+Console.WriteLine($"Square {number} = {result}"); // вывод результата на экран
