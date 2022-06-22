1a030408059d78715bd744a657811dd8b7381dfe
Gowaa <gowaa@bk.ru>
2022-06-22 15:53:51 +0300

    Add lesson 4

---
+++ lesson004/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 3. Напишите программу вычисления функции:
+ x = f(a)
+ Вычисления функции куба числа.
+ */
+
+Console.Write("Введите число ");
+int numA = int.Parse(Console.ReadLine());
+int kub = numA * numA * numA;
+
+Console.WriteLine($"куб числа равен {kub}");
